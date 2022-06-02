374b5276741da2a13da5a4cec8129384965bf335
mdsung <mdskylover@gmail.com>
2022-06-02 14:04:19 +0900

    Revise main.py to download with multithread

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ from pathlib import Path
 
 from tqdm import tqdm
 
-from src.gdrive import (
-    Credentials,
-    GDriveCredential,
-    GDriveFileDownloader,
-    GDriveReader,
-)
+from src.gdrive import Credentials, GDriveCredential, GDriveFileDownloader, GDriveReader
 
 DATA_PATH = Path("/home/data/tomocube")
 PROJECT_LIST = {
@@ -25,25 +20,33 @@ def check_file_exist(target_path, image):
     return Path(target_path, image["name"]).exists()
 
 
+def download_file(credentials, file_id, target_path, image_name):
+    downloader = GDriveFileDownloader(credentials)
+    downloader.download(file_id, target_path, image_name)
+
+
 def download_files(credentials: Credentials, target_path: Path, folder_id: str):
     image_raw_list = GDriveReader(credentials, folder_id).read()
-    downloader = GDriveFileDownloader(credentials)
     image_filter_list = [
         image
         for image in image_raw_list
         if not check_file_exist(target_path, image)
     ]
-    for image in tqdm(image_filter_list):
-        downloader.download(image["id"], target_path, image["name"])
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-    #     futures = [
-    #         executor.submit(
-    #             downloader.download, image["id"], target_path, image["name"]
-    #         )
-    #         for image in image_filter_list
-    #     ]
-    #     for future in concurrent.futures.as_completed(futures):
-    #         future.result()
+    # for image in tqdm(image_filter_list):
+    #     downloader.download(image["id"], target_path, image["name"])
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        futures = [
+            executor.submit(
+                download_file,
+                credentials,
+                image["id"],
+                target_path,
+                image["name"],
+            )
+            for image in image_filter_list
+        ]
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
 
 
 def main():
